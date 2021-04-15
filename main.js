21a9417e16f3d39939efab2807e1e57569d11183
ED-IP <edip_courses@outlook.es>
2021-04-15 13:34:20 +0000

    added rows to the table

--- main.js
+++ main.js
@@ -24,16 +24,23 @@ function getTableHeaders(obj) {
 }
 
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
+    
     getData(type, function (data) {
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function (item) {
-            // el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow);
         });
 
-        el.innerHTML = `<table>${tableHeaders}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
